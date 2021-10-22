cfefab0c0bac55ea886f8524699450fabc8a57df
zplus <zzz1362194009@163.com>
2021-10-22 12:57:57 +0800

    增加IoInfo

---
+++ IoInfo.cpp
@@ -0,0 +1,43 @@
+#include "IoInfo.h"
+
+
+void IoInfo::getCurrentInfo(){
+    this->read_block=0;
+    this->write_block=0;
+    this->read_tps=0;
+    this->write_block=0;
+
+    char cmd[] ="iostat -d -x";
+    char buffer[MAXBUFSIZE];
+    char a[20];
+    float arr[20];
+    FILE* pipe = popen(cmd, "r");
+    if (!pipe)  return ;
+    fgets(buffer, sizeof(buffer), pipe);
+    fgets(buffer, sizeof(buffer), pipe);
+    fgets(buffer, sizeof(buffer), pipe);
+    fgets(buffer, sizeof(buffer), pipe);
+    io_occupy_t temp;
+    while (strcmp("\n",buffer)!=0)
+    {
+        sscanf(buffer,"%s %f %f %f %f ",temp.name,&temp.read_tps, &temp.write_tps,&temp.read_block,&temp.write_block);    
+        this->read_tps+=temp.read_tps;
+        this->read_block+=temp.read_block;
+        this->write_tps+=temp.write_tps;
+        this->write_block+=temp.write_block;
+        fgets(buffer, sizeof(buffer), pipe);
+    }
+
+
+    pclose(pipe);
+    return ;
+    
+}
+
+
+void IoInfo::getInfoString(char * retMessage,int msgLen){
+    memset(retMessage,'\0',msgLen);
+    sprintf(retMessage,"每秒读IO次数：%8.2f\t每秒写IO次数：%8.2f\t每秒读KB数：%8.2f\t每秒写KB数：%8.2f\n", 
+                    this->read_tps, this->write_block, this->read_block, this->write_tps);
+    
+}
---
+++ IoInfo.h
@@ -0,0 +1,13 @@
+
+class IoOccupy{
+private:
+    char name[20];     
+    float read_tps; //每秒钟发送到的I/O读请求数
+    float write_tps; //每秒钟发送到的I/O写请求数
+    float read_block; //每秒读取的block数
+    float write_block; //每秒写入的block数
+public:
+    void getCurrentInfo();
+    void getInfoString(char * retMessage,int msgLen);
+}
+
