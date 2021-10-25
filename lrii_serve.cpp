d62031bcb16e8d61666d2fb6abe62b4fe8026ebb
zplus <zzz1362194009@163.com>
2021-10-25 10:07:36 +0800

    服务器增加监听

--- lrii_serve.cpp
+++ lrii_serve.cpp
@@ -4,6 +4,15 @@
 #include <sys/epoll.h>
 #include <stdlib.h>
 #include <fcntl.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <assert.h>
+#include <errno.h>
+#include <stdio.h>
+#include <string.h>
+
+#define MAX_EVENT_MEMBER 5
+#define TCP_BUFFER_SIZE 256
 
 int setnonblocking(int fd){
     int old_option = fcntl(fd,F_GETFL);
@@ -12,7 +21,7 @@ int setnonblocking(int fd){
     return old_option;
 }
 
-void addfd(int epollfd,int fd,bool enable_et){
+void addfd(int epollfd,int fd,bool enable_et=true){
     epoll_event event;
     event.data.fd=fd;
     event.events = EPOLLIN;
@@ -23,6 +32,76 @@ void addfd(int epollfd,int fd,bool enable_et){
     setnonblocking(fd);
 }
 
-int main(){
+int main(int argc ,char* argv[]){
+    if(argc<=2)
+    {
+        printf("the parameter is incorrect\n");
+        return 1;
+    }
+
+    const char *ip =argv[1];
+    int port = atoi(argv[2]);
+
+    int ret=0;
+
+    struct sockaddr_in address;
+    bzero(&address,sizeof(address));
+    address.sin_family=AF_INET;
+    inet_pton(AF_INET,ip,&address.sin_addr);
+    address.sin_port = htons(port);
+
+    int listen_fd = socket(PF_INET,SOCK_STREAM,0);
+    assert(listen_fd>=0);
+
+    ret =bind(listen_fd,(struct sockaddr *)&address,sizeof(address) );
+    assert(ret!=-1);
+
+    ret = listen(listen_fd,5);
+    assert(ret!=-1);
+
+    epoll_event events[MAX_EVENT_MEMBER];
+    int epoll_fd = epoll_create(5);
+    assert(ret!=-1);
+    addfd(epoll_fd,listen_fd,true);
+
+    while(1){
+        int number = epoll_wait(epoll_fd,events,MAX_EVENT_MEMBER,-1);
+        if(number<0){
+            printf("epoll failure\n");
+            break;
+        }
+        for(int i=0;i<number;i++){
+            int sockfd= events[i].data.fd;
+            if(sockfd==listen_fd){
+                struct sockaddr_in sock_client ;
+                socklen_t client_addr_length = sizeof(sock_client);
+                int connfd = accept(listen_fd, (struct sockaddr *)&sock_client,&client_addr_length);
+                if(connfd < 0){
+                    printf("accept a conn failure ,the errno is %d\n",errno);
+                    continue;
+                }
+                addfd(epoll_fd,connfd);
+            }else if(events[i].events & EPOLLIN){
+                char buf[TCP_BUFFER_SIZE];
+                while(1){
+                    memset(buf,'\0',TCP_BUFFER_SIZE);
+                    ret = recv(sockfd,buf,TCP_BUFFER_SIZE-1,0);
+                    if(ret < 0){
+                        if(errno==EAGAIN || errno== EWOULDBLOCK){
+                            break;
+                        }
+                        close(sockfd);
+                        break;
+                    }
+                }
+
+            }
+        }
+
+    }
+
+
+
+
 
 }
